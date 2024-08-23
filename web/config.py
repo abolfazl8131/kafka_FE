b65297fb7dc3968b5c5634ba9cc0e0dff7501142
abolfazl1381 <abolfazlandalib@gmail.com>
2024-08-23 10:41:27 +0330

    add .env to each module

--- web/config.py
+++ web/config.py
@@ -1,13 +1,9 @@
+import os
+
 config = {
     
-    'mongo': {
-        'host':'localhost',
-        'port':27017,
-        'db':'test',
-        'sparkuri':'mongodb://localhost:27017/test.test'
-    },
     'kafka':{
-        'host':'localhost',
+        'host':os.environ.get('KAFKA_HOST'),
         'port':9092,
         'group-id':'kafka-stream',
         'topic':'AB123',
